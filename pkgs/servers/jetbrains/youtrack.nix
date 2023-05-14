3c4c8881d63710847c95023fd14356c9343b2083
Leona Maroni <dev@leona.is>
2023-05-14 14:48:08 +0200

    youtrack: 2022.3.65371 -> 2023.1.9570

--- pkgs/servers/jetbrains/youtrack.nix
+++ pkgs/servers/jetbrains/youtrack.nix
@@ -1,30 +1,39 @@
-{ lib, stdenv, fetchurl, makeWrapper, jdk17, gawk }:
+{ lib, stdenv, fetchzip, makeWrapper, jdk17, p7zip, gawk, statePath ? "/var/lib/youtrack" }:
 
 stdenv.mkDerivation rec {
   pname = "youtrack";
-  version = "2022.3.65371";
+  version = "2023.1.9570";
 
-  jar = fetchurl {
-    url = "https://download.jetbrains.com/charisma/${pname}-${version}.jar";
-    sha256 = "sha256-NQKWmKEq5ljUXd64zY27Nj8TU+uLdA37chbFVdmwjNs=";
+  src = fetchzip {
+    url = "https://download.jetbrains.com/charisma/youtrack-${version}.zip";
+    sha256 = "sha256-vDwkwk15PBe1JQ3nOMZY/at5nxRYjzFbMl3JGULmdpE=";
   };
 
-  nativeBuildInputs = [ makeWrapper ];
-
-  dontUnpack = true;
+  nativeBuildInputs = [ makeWrapper p7zip ];
 
   installPhase = ''
     runHook preInstall
-    makeWrapper ${jdk17}/bin/java $out/bin/youtrack \
-      --add-flags "\$YOUTRACK_JVM_OPTS -jar $jar" \
-      --prefix PATH : "${lib.makeBinPath [ gawk ]}" \
+
+    mkdir -p $out/app
+    cp -a . $out/app
+
+    for path in "backups" "conf" "data" "logs" "temp"
+    do
+      rm -rf $out/app/$path
+      ln -s ${statePath}/$path $out/app/$path
+    done
+
+    makeWrapper "$out/app/bin/youtrack.sh" "$out/bin/youtrack" \
+      --prefix PATH : "$out/libexec/app:${lib.makeBinPath [ jdk17 gawk ]}" \
+      --set FJ_JAVA_EXEC "${jdk17}/bin/java" \
       --set JRE_HOME ${jdk17}
+
     runHook postInstall
   '';
 
   meta = with lib; {
     description = "Issue tracking and project management tool for developers";
-    maintainers = teams.serokell.members;
+    maintainers = teams.serokell.members ++ (with maintainers; [ leona ]);
     sourceProvenance = with sourceTypes; [ binaryBytecode ];
     # https://www.jetbrains.com/youtrack/buy/license.html
     license = licenses.unfree;
