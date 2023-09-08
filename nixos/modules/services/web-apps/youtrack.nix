e06c0d558edf8d7bdd2eecbe3160e72e114fdf39
Leona Maroni <dev@leona.is>
2023-09-08 17:42:53 +0200

    nixos/youtrack: rebuild module for 2023.1

--- nixos/modules/services/web-apps/youtrack.nix
+++ nixos/modules/services/web-apps/youtrack.nix
@@ -1,137 +1,177 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
   cfg = config.services.youtrack;
-
-  extraAttr = concatStringsSep " " (mapAttrsToList (k: v: "-D${k}=${v}") (stdParams // cfg.extraParams));
-  mergeAttrList = lib.foldl' lib.mergeAttrs {};
-
-  stdParams = mergeAttrList [
-    (optionalAttrs (cfg.baseUrl != null) {
-      "jetbrains.youtrack.baseUrl" = cfg.baseUrl;
-    })
-    {
-    "java.aws.headless" = "true";
-    "jetbrains.youtrack.disableBrowser" = "true";
-    }
-  ];
+  
+  package = cfg.package.override {
+    baseStatePath = cfg.statePath;
+  };
 in
 {
+  imports = [
+    (lib.mkRenamedOptionModule [ "services" "youtrack" "baseUrl" ] [ "services" "youtrack" "settings" "base-url" ])
+  ];
+
   options.services.youtrack = {
 
-    enable = mkEnableOption (lib.mdDoc "YouTrack service");
+    enable = lib.mkEnableOption (lib.mdDoc "YouTrack service");
 
-    address = mkOption {
+    address = lib.mkOption {
       description = lib.mdDoc ''
         The interface youtrack will listen on.
       '';
       default = "127.0.0.1";
-      type = types.str;
+      type = lib.types.str;
     };
 
-    baseUrl = mkOption {
-      description = lib.mdDoc ''
-        Base URL for youtrack. Will be auto-detected and stored in database.
-      '';
-      type = types.nullOr types.str;
-      default = null;
-    };
-
-    extraParams = mkOption {
-      default = {};
-      description = lib.mdDoc ''
-        Extra parameters to pass to youtrack. See
-        https://www.jetbrains.com/help/youtrack/standalone/YouTrack-Java-Start-Parameters.html
-        for more information.
-      '';
-      example = literalExpression ''
-        {
-          "jetbrains.youtrack.overrideRootPassword" = "tortuga";
-        }
-      '';
-      type = types.attrsOf types.str;
-    };
-
-    package = mkOption {
+    package = lib.mkOption {
       description = lib.mdDoc ''
         Package to use.
       '';
-      type = types.package;
-      default = pkgs.youtrack;
-      defaultText = literalExpression "pkgs.youtrack";
+      type = lib.types.package;
+      relatedPackages = [ "youtrack_2022_3" "youtrack_2023_1" ];
     };
 
-    port = mkOption {
+    port = lib.mkOption {
       description = lib.mdDoc ''
         The port youtrack will listen on.
       '';
       default = 8080;
-      type = types.port;
+      type = lib.types.port;
     };
 
-    statePath = mkOption {
+    statePath = lib.mkOption {
       description = lib.mdDoc ''
-        Where to keep the youtrack database.
+        Path were the YouTrack state is stored.
+        To this path the base version (e.g. 2023_1) of the used package will be appended.
       '';
-      type = types.path;
+      type = lib.types.path;
       default = "/var/lib/youtrack";
     };
 
-    virtualHost = mkOption {
+    virtualHost = lib.mkOption {
       description = lib.mdDoc ''
         Name of the nginx virtual host to use and setup.
         If null, do not setup anything.
       '';
       default = null;
-      type = types.nullOr types.str;
+      type = lib.types.nullOr lib.types.str;
     };
 
-    jvmOpts = mkOption {
+    jvmOpts = lib.mkOption {
       description = lib.mdDoc ''
         Extra options to pass to the JVM.
         See https://www.jetbrains.com/help/youtrack/standalone/Configure-JVM-Options.html
         for more information.
       '';
-      type = types.separatedString " ";
-      example = "-XX:MetaspaceSize=250m";
+      type = lib.types.separatedString " ";
+      example = "--J-XX:MetaspaceSize=250m";
       default = "";
     };
 
-    maxMemory = mkOption {
+    maxMemory = lib.mkOption {
       description = lib.mdDoc ''
         Maximum Java heap size
       '';
-      type = types.str;
+      type = lib.types.str;
       default = "1g";
     };
 
-    maxMetaspaceSize = mkOption {
+    maxMetaspaceSize = lib.mkOption {
       description = lib.mdDoc ''
         Maximum java Metaspace memory.
       '';
-      type = types.str;
+      type = lib.types.str;
       default = "350m";
+    };
+
+    settings = lib.mkOption {
+      type = with lib.types; attrsOf (oneOf [ bool int str ]);
+      description = lib.mdDoc ''
+        Settings directly set in a YouTrack configuration file.
+        See https://www.jetbrains.com/help/youtrack/server/2023.1/YouTrack-Java-Start-Parameters.html
+        for more information.
+      '';
+      example = {
+        tls-redirect-from-http = true;
+        jetbrains.youtrack.admin.restore = true;
+      };
+      default = {};
     };
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
+    # XXX: Drop all version feature switches at the point when we consider YT 2022.3 as outdated.
+    services.youtrack.package = with pkgs;
+      lib.mkDefault (
+        if lib.versionOlder config.system.stateVersion "23.11" then youtrack_2022_3
+        else youtrack_2023_1
+      );
 
-    systemd.services.youtrack = {
-      environment.HOME = cfg.statePath;
-      environment.YOUTRACK_JVM_OPTS = "${extraAttr}";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      path = with pkgs; [ unixtools.hostname ];
-      serviceConfig = {
-        Type = "simple";
-        User = "youtrack";
-        Group = "youtrack";
-        Restart = "on-failure";
-        ExecStart = ''${cfg.package}/bin/youtrack --J-Xmx${cfg.maxMemory} --J-XX:MaxMetaspaceSize=${cfg.maxMetaspaceSize} ${cfg.jvmOpts} ${cfg.address}:${toString cfg.port}'';
-      };
+    services.youtrack.settings = {
+      listen-address = cfg.address;
+      listen-port = toString cfg.port;
     };
+
+    systemd.services.youtrack = let
+      service_2022_3 = let
+        mergeAttrList = lib.foldl' lib.mergeAttrs {};
+        stdParams = mergeAttrList [
+          (lib.optionalAttrs (cfg.settings ? base-url && cfg.settings.base-url != null) {
+            "jetbrains.youtrack.baseUrl" = cfg.settings.base-url;
+          })
+          {
+          "java.aws.headless" = "true";
+          "jetbrains.youtrack.disableBrowser" = "true";
+          }
+        ];
+        extraAttr = lib.concatStringsSep " " (lib.mapAttrsToList (k: v: "-D${k}=${v}") (stdParams // cfg.settings));
+      in {
+        environment.HOME = cfg.statePath;
+        environment.YOUTRACK_JVM_OPTS = "${extraAttr}";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        path = with pkgs; [ unixtools.hostname ];
+        serviceConfig = {
+          Type = "simple";
+          User = "youtrack";
+          Group = "youtrack";
+          Restart = "on-failure";
+          ExecStart = ''${package}/bin/youtrack --J-Xmx${cfg.maxMemory} --J-XX:MaxMetaspaceSize=${cfg.maxMetaspaceSize} ${cfg.jvmOpts} ${cfg.address}:${toString cfg.port}'';
+        };
+      };
+      service_2023_1 = {
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        path = with pkgs; [ unixtools.hostname ];
+        preStart = ''
+          # This detects old (i.e. <= 2022.3) installations that were not migrated yet
+          # and migrates them to the new state directory style
+          if [[ -d ${cfg.statePath}/teamsysdata ]] && [[ ! -d ${cfg.statePath}/2022_3 ]]
+          then
+            mkdir -p ${cfg.statePath}/2022_3
+            mv ${cfg.statePath}/teamsysdata ${cfg.statePath}/2022_3
+            mv ${cfg.statePath}/.youtrack ${cfg.statePath}/2022_3
+          fi
+          mkdir -p ${cfg.statePath}/${package.baseVersion}/{backups,conf,data,logs,temp}
+        '' + lib.optionalString (lib.versionAtLeast package.version "2023.1") ''
+          ${package}/bin/youtrack configure -J-Ddisable.configuration.wizard.on.upgrade=true
+          ${package}/bin/youtrack configure --listen-port=${toString cfg.port} ${lib.concatStringsSep " " (lib.mapAttrsToList (name: value: "--${name}=${toString value}") cfg.settings )}
+        '';
+        serviceConfig = lib.mkMerge [
+          {
+            Type = "simple";
+            User = "youtrack";
+            Group = "youtrack";
+            Restart = "on-failure";
+            ExecStart = "${package}/bin/youtrack run --J-Xmx${cfg.maxMemory} --J-XX:MaxMetaspaceSize=${cfg.maxMetaspaceSize} ${cfg.jvmOpts}";
+          }
+          (lib.mkIf (cfg.statePath == "/var/lib/youtrack") {
+            StateDirectory = "youtrack youtrack/${package.baseVersion}";
+          })
+        ];
+      };
+    in if lib.versionAtLeast package.version "2023.1" then service_2023_1 else service_2022_3;
 
     users.users.youtrack = {
       description = "Youtrack service user";
@@ -143,7 +183,7 @@ in
 
     users.groups.youtrack = {};
 
-    services.nginx = mkIf (cfg.virtualHost != null) {
+    services.nginx = lib.mkIf (cfg.virtualHost != null) {
       upstreams.youtrack.servers."${cfg.address}:${toString cfg.port}" = {};
       virtualHosts.${cfg.virtualHost}.locations = {
         "/" = {
@@ -173,9 +213,9 @@ in
             proxy_set_header X-Forwarded-Proto $scheme;
           '';
         };
-
       };
     };
-
   };
+
+  meta.maintainers = [ lib.maintainers.leona ];
 }
